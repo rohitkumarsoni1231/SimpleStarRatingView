b8aa9f25092caf01c0b654497921f62b55bde4c2
Rohit Kumar <rohit.kumar@xynotech.com>
2022-12-28 13:52:27 +0500

    Initial Commit

---
+++ SimpleStarRatingView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleStarRatingView
+//
+//  Created by Rohit Kumar on 12/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
